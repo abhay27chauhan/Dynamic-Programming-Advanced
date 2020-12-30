9dc306a826d7a91f720a85e5b3778fe08674a6ec
ABHAY CHAUHAN <42849043+abhay27chauhan@users.noreply.github.com>
2020-12-30 14:08:29 +0530

    catalan Numbers

---
+++ catalanNumbers.java
@@ -0,0 +1,18 @@
+class catalanNumbers{
+
+    public static void main(String[] args){
+        int n = 4;
+
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<dp.length; i++){
+            for(int j=0; j<i; j++){
+                dp[i] += dp[j]*dp[i-1-j];
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
